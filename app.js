d192bcb60386f5bdb7a6639aa7f163f313fa4620
praveen dodla <dodlapraveen09@gmail.com>
2021-07-05 11:34:40 +0530

    initial commit

--- app.js
+++ app.js
@@ -1,37 +1,53 @@
-var express = require("express");
-var app = express();
-var port = 3000;
-var bodyParser = require('body-parser');
+const express = require("express");
+const app = express();
+const port = 8000;
+const bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-var mongoose = require("mongoose");
+const mongoose = require("mongoose");
 mongoose.Promise = global.Promise;
-mongoose.connect("mongodb://localhost:27017/formDB", { useNewUrlParser: true, useUnifiedTopology: true });
-var nameSchema = new mongoose.Schema({
+mongoose.connect("mongodb+srv://admin-praveen:test123@cluster0.dsmzc.mongodb.net/formDB", { useNewUrlParser: true, useUnifiedTopology: true });
+const nameSchema = new mongoose.Schema({
     firstName: String,    
     phoneName:Number,
     emailName:String
 
 
 });
-var User = mongoose.model("User", nameSchema);
+const User = mongoose.model("User", nameSchema);
 
 app.get("/", (req, res) => {
     res.sendFile(__dirname + "/index.html");
 });
 
 app.post("/addname", (req, res) => {
-    var myData = new User(req.body);
-    myData.save()
-        .then(item => {
-            res.send("Name saved to database");
-        })
-        .catch(err => {
-            res.status(400).send("Unable to save to database");
-        });
+    User.find({$or: [ { emailName: req.body.emailName}, { phoneName: req.body.phoneName} ] },(err,doc) => {
+        if(err){
+            res.send(err)
+        }else{
+            if(doc.length){
+                res.send("Emai or Mobile alredy existed")
+            }else{
+               const myData = new User(req.body);
+               myData.save()
+                   .then(item => {
+                       res.send("Name saved to database");
+                   })
+                   .catch(err => {
+                       res.status(400).send("Unable to save to database");
+                   });
+            }
+        }
+       
+   
+   })
 });
 
 app.listen(port, () => {
     console.log("Server listening on port " + port);
 });
+
+
+
+
